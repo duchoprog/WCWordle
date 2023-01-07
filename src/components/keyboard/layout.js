c0e6bb6e6b11fa0f9b5fbf11975b15ad13cd7f60
duchoprog <duchoprog@gmai.com>
2023-01-07 12:36:45 -0300

    updated colors

--- src/components/keyboard/layout.js
+++ src/components/keyboard/layout.js
@@ -2,6 +2,5 @@ export const layout =[
    [{text:'Q', color:""},{text:'W', color:""},{text:'E', color:""},{text:'R', color:""},{text:'T', color:""},{text:'Y', color:""},{text:'U', color:""},{text:'I', color:""},{text:'O', color:""},{text:'P', color:""}],
     [{text:'A', color:""},{text:'S', color:""},{text:'D', color:""},{text:'F', color:""},{text:'G', color:""},{text:'H', color:""},{text:'J', color:""},{text:'K', color:""},{text:'L', color:""}],
     [{text:'Z', color:""},{text:'X', color:""},{text:'C', color:""},{text:'V', color:""},{text:'B', color:""},{text:'N', color:""},{text:'M', color:""}],
-    [{width:3,text:'Enter'},{width:3, text:'Backspace'}]
-
+    [{width:3, text:'Backspace'}, {width:3,text:'Send'}]
 ]
