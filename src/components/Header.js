6d216a6451c0b7c528409534f48412a0d818da07
duchoprog <duchoprog@gmai.com>
2023-03-09 14:35:37 -0300

    fix title error

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { IoIosStats, IoMdInformationCircleOutline } from "react-icons/io";
 const Header = ({setIsOpen, setInfoIsOpen}) => {
   return (
     <div className='header'>
-        <h1>Ordle</h1> 
+        <h1>WCWordle</h1> 
         <div className="icons">
           <IoIosStats className="icon" onClick={()=>{setIsOpen(true)}} />
           <IoMdInformationCircleOutline className="icon" onClick={() => setInfoIsOpen(true)} />
