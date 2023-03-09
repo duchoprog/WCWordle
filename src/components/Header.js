af5bc9a68b113d6680e2d4918178d688d9d74237
duchoprog <duchoprog@gmai.com>
2023-03-09 15:07:48 -0300

    fixed instructions

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = ({setIsOpen, setInfoIsOpen}) => {
   return (
     <div className='header'>
         <h1>WCWordle</h1> 
+        <h4>Guess a FIFA WC 2022 player</h4>
         <div className="icons">
           <IoIosStats className="icon" onClick={()=>{setIsOpen(true)}} />
           <IoMdInformationCircleOutline className="icon" onClick={() => setInfoIsOpen(true)} />
--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -15,8 +15,8 @@ function InfoModal({setInfoIsOpen}) {
                         <h3 style={{ marginBottom: "-3px" }}> X </h3>
                     </button>
                     <div className={styles.info}>
-                        <p>Try to guess the 5 letter ANSWER</p>
-                        <p>Type a 5 letter word, and press send.</p>
+                        <p>Try to guess the 5 letter ANSWER, which is the last name of a FIFA WC 2022</p>
+                        <p>To guess, type a 5 letter word, and press send. It may be any English word, or a FIFA WC 2022 player's last name.</p>
                         <p>Letters that turn yellow are used in the ANSWER, but are in the wrong position.</p>
                         <p>Letters that turn green are used in the ANSWER, and are in the right position.</p>
                         <p>Good luck!</p>
